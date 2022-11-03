3f048c71c20367e1c817056a43714d9f5768b330
Lucas Prieto <lucasprieto30@gmail.com>
2022-11-03 20:40:13 -0300

    fix mayusculas create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS tarjetas;
 CREATE DATABASE tarjetas;
 \c tarjetas
-CREATE TABLE cliente (nrocliente int, nombre text, apellido text, domicilio text, telefono char(12));
-CREATE TABLE tarjeta (nrotarjeta char(16), nrocliente int, validadesde char(6), validahasta char(6), codseguridad char(4), limitecompra decimal(8,2), estado char(10));
-CREATE TABLE comercio (nrocomercio int, nombre text, domicilio text, codigopostal char(8), telefono char(12));
-CREATE TABLE compra (nrooperacion int, nrotarjeta char(16), nrocomercio int, fecha timestamp, monto decimal(7,2), pagado boolean);
-CREATE TABLE rechazo (nrorechazo int, nrotarjeta char(16), nrocomercio int, fecha timestamp, monto decimal(7,2), motivo text);
-CREATE TABLE cierre (año int, mes int, terminacion int, fechainicio date, fechacierre date, fechavto date);
-CREATE TABLE cabecera (nroresumen int, nombre text, apellido text, domicilio text, nrotarjeta char(16), desde date, hasta date, vence date, total decimal(8,2));
-CREATE TABLE detalle (nroresumen int, nrolinea int, fecha date, nombrecomercio text, monto decimal(7,2));
-CREATE TABLE alerta (nroalerta int, nrotarjeta char(16), fecha timestamp, nrorechazo int, codalerta int, descripcion text);
-CREATE TABLE consumo (nrotarjeta char(16), codseguridad char(4), nrocomercio int, monto decimal(7,2));
+CREATE TABLE cliente (nrocliente INT, nombre TEXT, apellido TEXT, domicilio TEXT, telefono CHAR(12));
+CREATE TABLE tarjeta (nrotarjeta CHAR(16), nrocliente INT, validadesde CHAR(6), validahasta CHAR(6), codseguridad CHAR(4), limitecompra DECIMAL(8,2), estado CHAR(10));
+CREATE TABLE comercio (nrocomercio INT, nombre TEXT, domicilio TEXT, codigopostal CHAR(8), telefono CHAR(12));
+CREATE TABLE compra (nrooperacion INT, nrotarjeta CHAR(16), nrocomercio INT, fecha TIMESTAMP, monto DECIMAL(7,2), pagado BOOLEAN);
+CREATE TABLE rechazo (nrorechazo INT, nrotarjeta CHAR(16), nrocomercio INT, fecha TIMESTAMP, monto DECIMAL(7,2), motivo TEXT);
+CREATE TABLE cierre (año INT, mes INT, terminacion INT, fechainicio DATE, fechacierre DATE, fechavto DATE);
+CREATE TABLE cabecera (nroresumen INT, nombre TEXT, apellido TEXT, domicilio TEXT, nrotarjeta CHAR(16), desde DATE, hasta DATE, vence DATE, total DECIMAL(8,2));
+CREATE TABLE detalle (nroresumen INT, nrolinea INT, fecha DATE, nombrecomercio TEXT, monto DECIMAL(7,2));
+CREATE TABLE alerta (nroalerta INT, nrotarjeta CHAR(16), fecha TIMESTAMP, nrorechazo INT, codalerta INT, descripcion TEXT);
+CREATE TABLE consumo (nrotarjeta CHAR(16), codseguridad CHAR(4), nrocomercio INT, monto DECIMAL(7,2));
