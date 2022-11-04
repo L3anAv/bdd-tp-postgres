889c5eea5828a48e561d6fbb1b025762ad6ed975
Gianni Perez <giannina-b-perez@hotmail.com>
2022-11-04 15:52:35 -0300

    Add tarjetas values

--- inserts.sql
+++ inserts.sql
@@ -1,41 +1,72 @@
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (0,'Matias', 'Avila','9 de Julio 2302','541112321232')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (1,'Giannina', 'Perez','Panamericana Km 36.5','541145678970')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (2,'Lucas', 'Prieto','Av. Pres. Arturo Umberto Illia 3770','541142335678')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (3,'Guadalupe', 'Torres','Av. Victorica 1128','541134521789')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (4,'Tomas', 'Gutierrez','Formosa y Ruta 52','541167789012')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (5,'Juan', 'Ugarte','Quevedo 3365','541146578974')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (6,'Sergio', 'Messi','Av. Del Libertador 6820','541156920932')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (7,'Santiago', 'Pereyra','Paraná 3745','541154648972')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (8,'Karina', 'Castan','San Martín 546','541176853412')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (9,'Emiliano', 'Ayala','Sarmiento 2157','541156748921')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (10,'Fabian', 'Moreno','Juan Julian Lastra 2400','541145769276')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (11,'Jorge', 'Peron','Av Nestor C. Kirchner 1142','541125678970')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (12,'Federico', 'Santillan','Josiah Williams 209','541178929283')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (13,'Sebastian', 'Rodriguez','Perito Moreno 1460','541176526341')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (14,'Camila', 'Martin','San Martín 800','541154327801')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (15,'Tania', 'Rojo','Av. España 309','541134679086')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (16,'Belen', 'Aristimuño','Av. Libertad 254','541197367361')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (17,'Morena', 'Dominguez','Av. Sáenz Peña 318','541121340968')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (18,'Paola', 'Tugas','Enrique Bodereau 7571','541197656291')
-INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (19,'Paulo', 'Gomez','Recta Martinoli 8357','541187561264')
+-- Clientes
 
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (0,'Ferreteria El Cosito', 'Cervantes 565','C1425EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (1,'Carniceria La Vaca Feliz', 'Gral. Guemes 897','B8225EID','5411');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (2,'Bar El Obrero', 'Donado 65','E6325EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (3,'Merceria Doña Tita', 'Pres. Juan Domingo Perón 4739','F3325EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (4,'Farmacia Balcarce', 'Paraná 3822','F6721EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (5,'Supermecado Los 3 Hermanos', 'Antonio Saenz 2041','B6721EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (6,'Hotel San Carlos', 'Av. Ing Agustín Rocca 249','G8902EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (7,'Dietetica El Obeso', 'Conquista Del Desierto 230','H9012EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (8,'Kiosco La Esquina', 'Dr. Salvador Sallares 63','F5462EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (9,'Verduleria Don Pedro', 'Bernardo De Monteagudo 3351','A7810EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (10,'Panaderia La Ponderosa', 'Brig. Gral. Juan M. De Rosas 14446','G2091EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (11,'Veterinaria Cura Bicho', 'Av. Eva Duarte de Peron 1474','H5674EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (12,'Tienda de Ropa La Gastadera', 'Hipólito Yrigoyen 1807','C4512EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (13,'Vidrieria El Reflejo', 'Av Luro 5975','F3241EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (14,'Gomeria La Rueda Feliz', '9 De Julio 1185','E4312EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (15,'Mecanica Austral', 'Calle 8 788','G3212EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (16,'Lavadero Blanca', 'Calle 12 esq. 58 Nº 1249','C6786EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (17,'Colchones Dulce Sueños', 'Sarmiento 2685','C2341EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (18,'Salon de Fiestas Wonka', 'Catamarca 1865','F0443EID','5411-0000-0000');
-INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (19,'Restaurant Ratatouille', 'Bartolomé Mitre 2642','G3245EID','5411-0000-0000');
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (0, 'Matias', 'Avila', '9 de Julio 2302', '541112321232')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (1, 'Giannina', 'Perez', 'Panamericana Km 36.5', '541145678970')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (2, 'Lucas', 'Prieto', 'Av. Pres. Arturo Umberto Illia 3770', '541142335678')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (3, 'Guadalupe', 'Torres', 'Av. Victorica 1128', '541134521789')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (4, 'Tomas', 'Gutierrez', 'Formosa y Ruta 52', '541167789012')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (5, 'Juan', 'Ugarte', 'Quevedo 3365', '541146578974')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (6, 'Sergio', 'Messi', 'Av. Del Libertador 6820', '541156920932')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (7, 'Santiago', 'Pereyra', 'Paraná 3745', '541154648972')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (8, 'Karina', 'Castan', 'San Martín 546', '541176853412')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (9, 'Emiliano', 'Ayala', 'Sarmiento 2157', '541156748921')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (10, 'Fabian', 'Moreno', 'Juan Julian Lastra 2400', '541145769276')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (11, 'Jorge', 'Peron', 'Av Nestor C. Kirchner 1142', '541125678970')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (12, 'Federico', 'Santillan', 'Josiah Williams 209', '541178929283')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (13, 'Sebastian', 'Rodriguez', 'Perito Moreno 1460', '541176526341')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (14, 'Camila', 'Martin', 'San Martín 800', '541154327801')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (15, 'Tania', 'Rojo', 'Av. España 309', '541134679086')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (16, 'Belen', 'Aristimuño', 'Av. Libertad 254', '541197367361')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (17, 'Morena', 'Dominguez', 'Av. Sáenz Peña 318', '541121340968')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (18, 'Paola', 'Tugas', 'Enrique Bodereau 7571', '541197656291')
+INSERT INTO cliente (nrocliente, nombre, apellido, domicilio, telefono) VALUES (19, 'Paulo', 'Gomez', 'Recta Martinoli 8357', '541187561264')
+
+-- Comercios
+
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (0, 'Ferreteria El Cosito', 'Cervantes 565','C1425EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (1, 'Carniceria La Vaca Feliz', 'Gral. Guemes 897','B8225EID','5411');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (2, 'Bar El Obrero', 'Donado 65','E6325EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (3, 'Merceria Doña Tita', 'Pres. Juan Domingo Perón 4739','F3325EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (4, 'Farmacia Balcarce', 'Paraná 3822','F6721EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (5, 'Supermecado Los 3 Hermanos', 'Antonio Saenz 2041','B6721EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (6, 'Hotel San Carlos', 'Av. Ing Agustín Rocca 249','G8902EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (7, 'Dietetica El Obeso', 'Conquista Del Desierto 230','H9012EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (8, 'Kiosco La Esquina', 'Dr. Salvador Sallares 63','F5462EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (9, 'Verduleria Don Pedro', 'Bernardo De Monteagudo 3351','A7810EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (10, 'Panaderia La Ponderosa', 'Brig. Gral. Juan M. De Rosas 14446','G2091EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (11, 'Veterinaria Cura Bicho', 'Av. Eva Duarte de Peron 1474','H5674EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (12, 'Tienda de Ropa La Gastadera', 'Hipólito Yrigoyen 1807','C4512EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (13, 'Vidrieria El Reflejo', 'Av Luro 5975','F3241EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (14, 'Gomeria La Rueda Feliz', '9 De Julio 1185','E4312EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (15, 'Mecanica Austral', 'Calle 8 788','G3212EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (16, 'Lavadero Blanca', 'Calle 12 esq. 58 Nº 1249','C6786EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (17, 'Colchones Dulce Sueños', 'Sarmiento 2685','C2341EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (18, 'Salon de Fiestas Wonka', 'Catamarca 1865','F0443EID','5411-0000-0000');
+INSERT INTO comercio (nrocomercio, nombre, domicilio, codigopostal, telefono) VALUES (19, 'Restaurant Ratatouille', 'Bartolomé Mitre 2642','G3245EID','5411-0000-0000');
+
+-- Tarjetas
+
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('9320584378172604', 0, '201106', '201406', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('4982394782623736', 1, '202008', '202308', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('5129043812284623', 2, '202203', '202503', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('1294382965767449', 3, '201912', '202212', '8246', 40000, 'suspendida');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('9293437257327169', 4, '202109', '202409', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('5827624652643290', 5, '202211', '202511', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('3928217404085943', 6, '202212', '202512', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('1982747364536562', 7, '201301', '201601', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('9012348282748326', 8, '202010', '202310', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('8274236578326572', 9, '201911', '202211', '8246', 40000, 'suspendida');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('8324732653627823', 10, '202204', '202504', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('7632462483439834', 11, '201212', '201512', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('7216435365643723', 12, '200112', '200412', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('3624376458982394', 13, '202007', '202307', '8246', 40000, 'suspendida');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('6347467826428439', 14, '202006', '202306', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('5923892848377829', 15, '201003', '201303', '8246', 40000, 'suspendida');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('3784736427463790', 16, '202003', '202303', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('3209340294208483', 17, '202108', '202408', '8246', 40000, 'suspendida');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('2743472943783934', 18, '202109', '202409', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('8437284736756736', 18, '200202', '200502', '8246', 40000, 'anulada');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('3464536272389793', 19, '202201', '202501', '8246', 40000, 'vigente');
+INSERT INTO tarjeta (nrotarjeta, nrocliente, validadesde, validahasta, codseguridad, limitecompra, estado) VALUES ('1273672467364703', 19, '200506', '200806', '8246', 40000, 'anulada');
+
+-- Cierres
