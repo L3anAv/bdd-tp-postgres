02d9594c2114be8b2ad74c61244426c1fc45b2b3
Gianni Perez <giannina-b-perez@hotmail.com>
2022-11-07 15:46:54 -0300

    Update generar_resumen()

--- create_database.sql
+++ create_database.sql
@@ -7,7 +7,7 @@ CREATE TABLE comercio (nrocomercio SERIAL, nombre TEXT, domicilio TEXT, codigopo
 CREATE TABLE compra (nrooperacion SERIAL, nrotarjeta CHAR(16), nrocomercio INT, fecha TIMESTAMP, monto DECIMAL(7,2), pagado BOOLEAN);
 CREATE TABLE rechazo (nrorechazo SERIAL, nrotarjeta CHAR(16), nrocomercio INT, fecha TIMESTAMP, monto DECIMAL(7,2), motivo TEXT);
 CREATE TABLE cierre (año INT, mes INT, terminacion INT, fechainicio DATE, fechacierre DATE, fechavto DATE);
-CREATE TABLE cabecera (nroresumen INT, nombre TEXT, apellido TEXT, domicilio TEXT, nrotarjeta CHAR(16), desde DATE, hasta DATE, vence DATE, total DECIMAL(8,2));
+CREATE TABLE cabecera (nroresumen SERIAL, nombre TEXT, apellido TEXT, domicilio TEXT, nrotarjeta CHAR(16), desde DATE, hasta DATE, vence DATE, total DECIMAL(8,2));
 CREATE TABLE detalle (nroresumen INT, nrolinea INT, fecha DATE, nombrecomercio TEXT, monto DECIMAL(7,2));
 CREATE TABLE alerta (nroalerta SERIAL, nrotarjeta CHAR(16), fecha TIMESTAMP, nrorechazo INT, codalerta INT, descripcion TEXT);
 CREATE TABLE consumo (nrotarjeta CHAR(16), codseguridad CHAR(4), nrocomercio INT, monto DECIMAL(7,2));
--- generar_resumen.sql
+++ generar_resumen.sql
@@ -1,71 +1,59 @@
---todas las compras del periodo + cabecera
-
---cabecera
---nroresumen:int,      (lo pongo yo)
---nombre:text,         (cliente)
---apellido:text,       (cliente)
---domicilio:text,      (cliente)
---nrotarjeta:char(16), (cliente)
---desde:date,          (cierre)
---hasta:date,          (cierre)
---vence:date,          (cierre)
---total:decimal(8,2)
-
---compra
---nrooperacion:int,
---nrotarjeta:char(16),
---nrocomercio:int,
---fecha:timestamp,     (si la fecha esta en el periodo, la uso para sumar su monto a aux_total)
---monto:decimal(7,2),
---pagado:boolean
-
-CREATE OR REPLACE FUNCTION generar_resumen (nro_cliente cliente.nrocliente%TYPE, año INT, mes INT) RETURNS void AS $$
+CREATE OR REPLACE FUNCTION generar_resumen (nro_cliente cliente.nrocliente%TYPE, aux_año INT, aux_mes INT) RETURNS void AS $$
 
         DECLARE
 
-	    aux_nroresumen INT := 0;
+	    
 	    aux_nrolinea INT := 1;
-	    aux_total INT := 0;
 	    aux_cliente RECORD;
 	    aux_compra RECORD;
 	    aux_tarjeta RECORD;
 	    aux_cierre RECORD;
 	    aux_comercio RECORD;
+            aux_nroresumen cabecera.nroresumen%type;
+            aux_total cabecera.total%type;
 
         BEGIN
 
-                INSERT INTO aux_cliente SELECT * FROM cliente WHERE nrocliente = nro_cliente; --guardo cliente en aux_cliente
-	        --falta si cliente no existe...
+                --guardo cliente en aux_cliente
+                SELECT * INTO aux_cliente FROM cliente WHERE nrocliente = nro_cliente;
+	                IF NOT FOUND THEN
+	      		        RAISE 'El número de cliente % no existe.', nro_cliente;
+  		        END IF;
 
-                FOR aux_tarjeta IN SELECT * FROM tarjeta WHERE nrocliente = aux_nrocliente LOOP --recorro las tarjetas del cliente
+                --recorro la o las tarjetas del cliente
+                FOR aux_tarjeta IN SELECT * FROM tarjeta WHERE nrocliente = aux_cliente.nrocliente LOOP
         
-		        aux_total := 0; --reinicio el total a pagar
+		        aux_total := 0; --reinicio total a pagar
 
-		        SELECT * INTO aux_cierre FROM cierre c WHERE c.año = año AND c.mes = mes; --guardo cierre en aux_cierre
+		        SELECT * INTO aux_cierre FROM cierre WHERE año = aux_año AND mes = aux_mes
+                                AND terminacion = substring(aux_tarjeta.nrotarjeta, 16, 1)::INT; --guardo cierre en aux_cierre
 
                         --creo cabecera
                         --falta aux_nroresumen y total = 0
                         INSERT INTO cabecera (nombre, apellido, domicilio, nrotarjeta, desde, hasta, vence, total)
                                 VALUES (aux_cliente.nombre, aux_cliente.apellido, aux_cliente.domicilio, aux_tarjeta.nrotarjeta, aux_cierre.fechainicio, aux_cierre.fechacierre, aux_cierre.fechavto, aux_total);
 
-                        --guardo nroresumen autogenerado en aux_nroresumen para usarlo en detalle
-                        INSERT INTO aux_nroresumen SELECT nroresumen FROM cabecera WHERE nrotarjeta = aux_tarjeta.nrotarjeta
+                        --guardo los nroresumen autogenerados en aux_nroresumen para usarlo en detalle
+                        INSERT INTO cabecera(nroresumen) SELECT nroresumen FROM cabecera WHERE nrotarjeta = aux_tarjeta.nrotarjeta
                                 AND desde = aux_cierre.fechainicio AND hasta = aux_cierre.fechacierre;
 
+                        --recorro compras
                         FOR aux_compra IN SELECT * FROM compra WHERE nrotarjeta = aux_tarjeta.nrotarjeta AND fecha >= aux_cierre.fechainicio AND fecha <= aux_cierre.fechacierre AND pagado = false LOOP
 
-                                SELECT DISTINCT * INTO aux_comercio FROM comercio WHERE nrocomercio = aux_compra.nrocomercio;
+                                --guardo comercio en aux_comercio
+                                SELECT * INTO aux_comercio FROM comercio WHERE nrocomercio = aux_compra.nrocomercio;
 
-                                --falta aux_nroresumen y aux_nrolinea
-                                INSERT INTO detalle (nroresumen, nrolinea, fecha, nombre_comercio, monto)
+                                --creo detalle
+                                INSERT INTO detalle (nroresumen, nrolinea, fecha, nombrecomercio, monto)
                                         VALUES (aux_nroresumen, aux_nrolinea, aux_compra.fecha, aux_comercio.nombre, aux_compra.monto);
-			        aux_nrolinea := aux_nrolinea + 1;
-                                aux_total := aux_total + aux_compra.monto;
-                                UPDATE compra SET pagado = true WHERE nrooperacion = aux_compra.nrooperacion;
+			        aux_nrolinea := aux_nrolinea + 1; --incremento aux_nrolinea
+                                aux_total := aux_total + aux_compra.monto; --incremento total
+                                UPDATE compra SET pagado = true WHERE nrooperacion = aux_compra.nrooperacion; --actualizo bool pagado
                         END LOOP;
 
-                        UPDATE cabecera SET total = aux_total WHERE nrotarjeta = aux_tarjeta.nrotarjeta;
+                        UPDATE cabecera SET total = aux_total WHERE nrotarjeta = aux_tarjeta.nrotarjeta --actualizo total en cabecera
+                                AND desde = aux_cierre.fechainicio AND hasta = aux_cierre.fechacierre;
                         
                 END LOOP;
-END;
+        END;
 $$ LANGUAGE plpgsql;
