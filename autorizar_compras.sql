8a56b2c3b9559ec75b08d04248543173fe10255d
Leandro Avila <leandroav.dev@gmail.com>
2022-11-09 18:30:35 -0300

    Modificaion de archivo autorizar_compras.sql

--- autorizar_compras.sql
+++ autorizar_compras.sql
@@ -11,19 +11,20 @@ DECLARE
 	monto_total_compras_tarjeta_actual compra.monto%type; -- Monto total de compras actuales de la tarjeta pasada por parametro.
 BEGIN
 
+    -- Seleccion de fila completa de la tarjeta pasada por nrotarjeta.
+    SELECT * INTO tarjeta_fila FROM tarjeta t WHERE n_tarjeta = t.nrotarjeta;
+    
     -- Seleccion de fecha de vencimiento de la tarjeta pasada por nrotarjeta.
-    SELECT CAST(validahasta AS TEXT) INTO fecha_de_vencimiento_text FROM tarjeta t WHERE n_tarjeta = t.nrotarjeta;
+    SELECT CAST(validadesde AS TEXT) INTO fecha_de_vencimiento_text FROM tarjeta t WHERE n_tarjeta = t.nrotarjeta;
 
 	--Asignacion a variables monto total de compras de tarjeta.
-	--monto_total_compra_tarjeta_actual := SELECT SUM(monto) FROM compra c WHERE n_tarjeta = c.nrotarjeta;
-	SELECT SUM(monto) INTO monto_total_compras_tarjeta_actual FROM compra c WHERE n_tarjeta = c.nrotarjeta and c.pagado = false;
+	monto_total_compras_tarjeta_actual := (SELECT SUM(monto) FROM compra c WHERE n_tarjeta = c.nrotarjeta);
+	--SELECT SUM(monto) INTO monto_total_compras_tarjeta_actual FROM compra c WHERE n_tarjeta = c.nrotarjeta and c.pagado = false;
    
     -- Conversion y Asignacion de fechas como type DATE
     fecha_actual := CURRENT_DATE;
 	fecha_vencimiento := TO_DATE(fecha_de_vencimiento_text, 'YYYYMM');
 	
-    -- Seleccion de fila completa de la tarjeta pasada por nrotarjeta.
-    SELECT * INTO tarjeta_fila  FROM tarjeta t WHERE t.nrotarjeta = n_tarjeta ;
     -- Control de la existencia de nrotarjeta pasada por parametro.
     IF NOT found then
         INSERT INTO rechazo (nrotarjeta, nrocomercio, fecha, monto, motivo) 
@@ -37,30 +38,29 @@ BEGIN
             VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, 'Codigo de seguridad invalido.');
         
         return false;
+    
+    --Control que el monto total de compras de la tarjeta no supere el limite permitido de la misma.
+    ELSIF monto_total_compras_tarjeta_actual >= tarjeta_fila.limitecompra then
+        INSERT INTO rechazo (nrotarjeta, nrocomercio, fecha, monto, motivo)
+            VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, 'Se supero el límite de la tarjeta.');
 
+        return false;
+    
     -- Control de que la tarjeta no este vencida.
-    ELSIF fecha_actual > fecha_vencimiento then
+    ELSIF fecha_vencimiento > fecha_actual then
         INSERT INTO rechazo (nrotarjeta, nrocomercio, fecha, monto, motivo)
             VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, 'Plazo de vigencia expirado.');
     
         return false;
 
-    --Control de tarjeta que no este suspendida
+    --Control de tarjeta que no este suspendida.
     ELSIF tarjeta_fila.estado = 'suspendida' then
         INSERT INTO rechazo (nrotarjeta, nrocomercio, fecha, monto, motivo)
             VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, 'La Tarjeta se encuentra suspendida.');
     
         return false;
-
-    --Control de monto que no supere el limite permitido de la tarjeta.
-
-    ELSIF tarjeta_fila.limitecompra < monto_total_compras_tarjeta_actual then
-        INSERT INTO rechazo (nrotarjeta, nrocomercio, fecha, monto, motivo)
-            VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, 'Supera límite de tarjeta');
     
-        return false;
-    
-    -- Paso controles entonces, se efectua la compra por autorizar y se inserta en la tabla correspondiente.
+    -- Si pasa todos los controles, se efectua la compra por autorizar y se inserta en la tabla correspondiente.
     ELSE
         INSERT INTO compra (nrotarjeta, nrocomercio, fecha, monto, pagado)
             VALUES (n_tarjeta, n_comercio, current_timestamp, monto_compra, false);
